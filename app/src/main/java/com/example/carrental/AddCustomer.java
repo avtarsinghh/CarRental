b28417bf1d68125a9188fab4492483c1dab7d0c1
Avtar Singh <avi.avtargill@gmail.com>
2019-10-24 00:14:41 -0400

    Add Customer coded

--- app/src/main/java/com/example/carrental/AddCustomer.java
+++ app/src/main/java/com/example/carrental/AddCustomer.java
@@ -22,18 +22,36 @@ public class AddCustomer extends AppCompatActivity {
         etPassword = findViewById(R.id.addCustomerPassword);
 
         addCustomer = findViewById(R.id.btnAddCustomer);
-
+        userRepository = UserRepository.getInstance();
         addCustomer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fName, lName, uName, password;
                 fName = etFirstName.getText().toString();
                 lName = etLastName.getText().toString();
-                uName = etUserName.getText().toString();
+                uName = etUserName.getText().toString().toLowerCase();
                 password = etPassword.getText().toString();
-
                 if(fName.equals("") || lName.equals("") || uName.equals("") || password.equals("")){
                     Toast.makeText(AddCustomer.this, "Fill all the credentials", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!userRepository.getUsers().containsKey(uName)){
+                        User user = new User();
+                        user.firstName = fName;
+                        user.lastName = lName;
+                        user.userName = uName;
+                        user.password = password;
+                        user.role = "client";
+                        userRepository.addUser(user);
+                        etFirstName.setText("");
+                        etLastName.setText("");
+                        etPassword.setText("");
+                        etUserName.setText("");
+                        Toast.makeText(AddCustomer.this, "Customer Added successfully", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(AddCustomer.this, "UserName already exists", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
