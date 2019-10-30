aa013e74db9131004d9742348d51613953ed197e
Amardeep Kaur <annukaur25101992@gmail.com>
2019-10-29 20:08:32 -0400

    Cars Details Activity is coded..

--- app/src/main/java/com/example/carrental/RentNow.java
+++ app/src/main/java/com/example/carrental/RentNow.java
@@ -3,12 +3,34 @@ package com.example.carrental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RentNow extends AppCompatActivity {
+
+    TextView txtType,txtBrand,txtModel,txtColor,txtYear,txtLicense;
+    Button btnRentNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_now);
+
+        txtType=findViewById(R.id.txtTypeActivityRentNow);
+        txtBrand=findViewById(R.id.txtBrandRentNow);
+        txtModel=findViewById(R.id.txtModelActivityRentNow);
+        txtColor=findViewById(R.id.txtColourActivityRentNow);
+        txtYear=findViewById(R.id.txtYearActivityRentNow);
+        txtLicense=findViewById(R.id.txtLicenceActivityRentNow);
+        btnRentNow=findViewById(R.id.btnRentActivityRentNow);
+
+        btnRentNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
     }
 }
